5f39cd3605e93306c5688e1c4577017275122336
OlegZhilo <oleg.zhilo@surfstudio.ru>
2018-03-17 22:29:15 +0300

    Some refactor

--- app/src/main/java/ru/ozh/draganddropit/MainActivity.kt
+++ app/src/main/java/ru/ozh/draganddropit/MainActivity.kt
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v4.view.ViewCompat
 import android.support.v7.widget.GridLayoutManager
+import android.support.v7.widget.SimpleItemAnimator
 import android.view.MotionEvent
 import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
@@ -43,36 +44,32 @@ class MainActivity : AppCompatActivity() {
 
     private val deleteDragAndDropHelper = DragAndDropHelper(
             onEntered = { v, _, e ->
-                if (e.clipDescription.label != "add") {
-                    scale(v, true)
-                }
+                doIf(e.clipDescription.label != "add",
+                        {
+                            scale(v, true)
+                        })
+
             },
             onExited = { v, _, e ->
-                if (e.clipDescription.label != "add") {
-                    scale(v, false)
-                }
+                doIf(e.clipDescription.label != "add",
+                        {
+                            scale(v, true)
+                        })
             },
             onDrop = { v, dg, e ->
-                if (e.clipDescription.label != "add") {
-                    val pojo = list.find { it.id == (dg.tag as Int) }
-                    list.remove(pojo)
-                    scale(v, false, {
-                        render()
-                    })
-                }
+                doIf(e.clipDescription.label != "add",
+                        {
+                            val element = list.find { it.id == (dg.tag as Int) }
+                            list.remove(element)
+                            scale(v, false, {
+                                render()
+                            })
+                        })
             }
     )
 
-    private fun scale(v: View, upScale: Boolean, f: (() -> Unit)? = null) {
-        ViewCompat.animate(v)
-                .scaleX(if (upScale) 1.2F else 1F)
-                .scaleY(if (upScale) 1.2F else 1F)
-                .setDuration(150)
-                .withEndAction(f)
-                .start()
-    }
-
     private val pojoController = PojoController(controllerDragAndDropHelper)
+
     private val easyAdapter = EasyAdapter()
 
     private val list = mutableListOf<SomePojo>()
@@ -87,16 +84,17 @@ class MainActivity : AppCompatActivity() {
 
         recycler.layoutManager = GridLayoutManager(this, 3)
         recycler.adapter = easyAdapter
+        (recycler.itemAnimator as SimpleItemAnimator).supportsChangeAnimations = false
 
-        increment_tv.setOnTouchListener(View.OnTouchListener { _, event ->
+        increment_view.setOnTouchListener(View.OnTouchListener { _, event ->
             when (event.action) {
                 MotionEvent.ACTION_DOWN -> {
                     val clipData = ClipData.newPlainText("add", "")
-                    val shadowBuilder = View.DragShadowBuilder(increment_tv)
+                    val shadowBuilder = View.DragShadowBuilder(increment_view)
                     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-                        increment_tv.startDragAndDrop(clipData, shadowBuilder, increment_tv, 0)
+                        increment_view.startDragAndDrop(clipData, shadowBuilder, increment_view, 0)
                     } else {
-                        increment_tv.startDrag(clipData, shadowBuilder, increment_tv, 0)
+                        increment_view.startDrag(clipData, shadowBuilder, increment_view, 0)
                     }
                     return@OnTouchListener true
                 }
@@ -105,6 +103,7 @@ class MainActivity : AppCompatActivity() {
         })
 
         delete_tv.setOnDragListener(deleteDragAndDropHelper)
+
         reset_btn.setOnClickListener {
             inflateValues()
             render()
@@ -125,6 +124,19 @@ class MainActivity : AppCompatActivity() {
                 add(SomePojo(id = index))
             }
         }
+    }
+
+    private fun scale(v: View, upScale: Boolean, f: (() -> Unit)? = null) {
+        ViewCompat.animate(v)
+                .scaleX(if (upScale) 1.2F else 1F)
+                .scaleY(if (upScale) 1.2F else 1F)
+                .setDuration(150)
+                .withEndAction(f)
+                .start()
+    }
+
+    private fun doIf(condition: Boolean, f: (() -> Unit)?) {
+        if (condition) f?.invoke()
     }
 
 }
